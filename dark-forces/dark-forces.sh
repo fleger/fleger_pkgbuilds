7f5387d51f95d79d7a5b320e173750b60e43c41e
Florian Léger <florian6.leger@laposte.net>
2010-08-18 15:12:31 +0200

    New: dark-forces-1.0-1 (no .desktop yet)

---
+++ dark-forces/dark-forces.sh
@@ -0,0 +1,54 @@
+# /bin/bash
+
+sysdir="/usr/share/games/dark-forces"
+usrdir="$HOME/.dark-forces"
+
+symlinks=(bootmkr.exe dark.exe dos4gw.exe drive.cd imuse.exe install.exe readme.txt)
+copies=(imuse.ini jedi.cfg local.msg)
+
+if [ ! -d "$usrdir" ]; then
+    mkdir -p "$usrdir"
+fi
+
+for f in "${symlinks[@]}"; do
+    cp -urs "$sysdir/dark/$f" "$usrdir" 2&> /dev/null
+done
+
+for f in "${copies[@]}"; do
+    cp -ur "$sysdir/dark/$f" "$usrdir" 2&> /dev/null
+done
+
+dosboxMainConfig="$(dosbox -printconf)"
+dosboxConfigBaseName="""$(basename "$dosboxMainConfig")"""
+
+if [ ! -e "$usrdir/$dosboxConfigBaseName" ]; then
+    cp "$dosboxMainConfig" "$usrdir/$dosboxConfigBaseName"
+fi
+
+batchScript="""
+mount -u c
+mount -u d
+mount c "$usrdir"
+mount d "$sysdir"
+c:
+cd \
+"""
+
+if [ "$1" = "--setup" ]; then
+    batchScript+="""
+install.exe
+"""
+elif [ ! -f "$usrdir/DARKPILO.CFG" ]; then
+    batchScript+="""
+install.exe
+dark.exe
+"""
+else
+    batchScript+="""
+dark.exe
+"""
+fi
+
+batchScript+="exit"
+
+dosbox -conf "$usrdir/$dosboxConfigBaseName" -c "$batchScript" -exit
