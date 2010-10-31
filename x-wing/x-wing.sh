535cc5a0ecd84b614e51e3cdea13d800fb825025
fleger <florian6.leger@laposte.net>
2010-10-31 11:33:26 +0100

    x-wing-1.0-1: addded missing x-wing.sh

---
+++ x-wing/x-wing.sh
@@ -0,0 +1,62 @@
+# /bin/bash
+
+shopt -s extglob
+
+sysdir="/usr/share/games/x-wing"
+usrdir="$HOME/.x-wing"
+firstRun=false
+
+declare -A symlinks=([!(topace5.plt|setmuse.ini|landru.cfg|options.cfg)]="xwingcd")
+declare -A copies=([topace5.plt setmuse.ini landru.cfg options.cfg]="xwingcd")
+
+
+if [ ! -d "$usrdir" ]; then
+    mkdir -p "$usrdir"
+    firstRun=true
+fi
+
+cd "$sysdir"
+
+for f in "${!symlinks[@]}"; do
+    mkdir -p "$usrdir/${symlinks[$f]}"
+    cp -urs "$sysdir/"$f "$usrdir/${symlinks[$f]}" #2> /dev/null
+done
+
+for f in "${!copies[@]}"; do
+    mkdir -p "$usrdir/${copies[$f]}"
+    cp -ur $f "$usrdir/${copies[$f]}"   #2> /dev/null
+done
+
+dosboxMainConfig="$(dosbox -printconf)"
+dosboxConfigBaseName="""$(basename "$dosboxMainConfig")"""
+
+if [ ! -e "$usrdir/$dosboxConfigBaseName" ]; then
+    cp "$dosboxMainConfig" "$usrdir/$dosboxConfigBaseName"
+fi
+
+batchScript="""
+mount -u c
+mount -u d
+mount -t cdrom d "$sysdir"
+mount c "$usrdir"
+"""
+
+if [ "$1" = "--setup" ] || $firstRun; then
+    batchScript+="""
+c:
+cd \\xwingcd
+install.exe
+"""
+fi
+
+if [ "$1" != "--setup" ]; then
+    batchScript+="""
+c:
+cd \\xwingcd
+bwing.exe
+"""
+fi
+
+batchScript+="exit"
+
+dosbox -conf "$usrdir/$dosboxConfigBaseName" -c "$batchScript" -exit
