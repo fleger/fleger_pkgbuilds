c94f06202fecba40d3f76ec5e70b92ba9baa4a2d
fleger <florian6.leger@laposte.net>
2011-02-15 21:59:19 +0100

    arkanoid-1.0-2: better launcher script

--- arkanoid/arkanoid.sh
+++ arkanoid/arkanoid.sh
@@ -1,38 +1,36 @@
-# /bin/bash
+#! /bin/bash
 
 shopt -s extglob
 
-game="arkanoid"
-sysdir="/usr/share/games/$game"
-usrdir="$HOME/.$game"
-firstRun=false
+## Begin configuration
 
+game="arkanoid"
+
+# Where the game files are installed
+sysdir="/usr/share/games/$game"
+
+# Where the user's game directory will be
+usrdir="$HOME/.$game"
+
+# List of files that should be symlinked. Syntax: [file1 file2...]="installation subdirectory"
 declare -A symlinks=([arkanoid.?(com|pgm)]="")
+
+# List of files that should be copied (writable files like scores, configuration...)
 declare -A copies=([arkanoid.scr]="")
 
-if [ ! -d "$usrdir" ]; then
-    mkdir -p "$usrdir"
-    firstRun=true
-fi
+# Batch scripts executed by DOSBox
 
-for f in "${!symlinks[@]}"; do
-    mkdir -p "$usrdir/${symlinks[$f]}"
-    cp -urs "$sysdir/"$f "$usrdir/${symlinks[$f]}" 2> /dev/null
-done
+# Always executed at the beginning
+beginScript=""
 
-for f in "${!copies[@]}"; do
-    mkdir -p "$usrdir/${copies[$f]}"
-    cp -nr "$sysdir/"$f "$usrdir/${copies[$f]}"   2> /dev/null
-done
+# Executed only if this is the first time the program is started
+firstRunScript=""
 
-dosboxMainConfig="$(dosbox -printconf)"
-dosboxConfigBaseName="""$(basename "$dosboxMainConfig")"""
+# Executed only if this is *not* the first time the program is started
+notFirstRunScript=""
 
-if [ ! -e "$usrdir/$dosboxConfigBaseName" ]; then
-    cp "$dosboxMainConfig" "$usrdir/$dosboxConfigBaseName"
-fi
-
-batchScript="""
+# Always executed at the end
+endScript="""
 mount -u c
 mount c "$usrdir"
 c:
@@ -40,6 +38,48 @@ cd \\
 arkanoid.com
 """
 
+## End configuration
+
+# Test if this is the first time the program is started
+firstRun=false
+if [ ! -d "$usrdir" ]; then
+  mkdir -p "$usrdir"
+  firstRun=true
+fi
+
+# Create / update symlinks
+for f in "${!symlinks[@]}"; do
+  mkdir -p "$usrdir/${symlinks[$f]}"
+  cp -urs "$sysdir/"$f "$usrdir/${symlinks[$f]}" 2> /dev/null
+done
+
+# Copy files
+for f in "${!copies[@]}"; do
+  mkdir -p "$usrdir/${copies[$f]}"
+  cp -nr "$sysdir/"$f "$usrdir/${copies[$f]}"   2> /dev/null
+done
+
+# Copy the general DOSBox configuration file to $usrdir
+dosboxMainConfig="$(dosbox -printconf)"
+dosboxConfigBaseName="""$(basename "$dosboxMainConfig")"""
+
+if [ ! -e "$usrdir/$dosboxConfigBaseName" ]; then
+  cp "$dosboxMainConfig" "$usrdir/$dosboxConfigBaseName"
+fi
+
+# Build the batch script that will be executed in the DOSBox
+batchScript="$beginScript"
+
+if firstRun; then
+  batchScript+="$firstRunScript"
+else
+  batchScript+="$notFirstRunScript"
+fi
+
+batchScript+="$endScript"
+
+# Exit the DOSBox after the program is finished
 batchScript+="exit"
 
+# Start the DOSBox
 dosbox -conf "$usrdir/$dosboxConfigBaseName" -c "$batchScript" -exit
